5e71047d93ee924426d4faa453b05011ae35e29e
protoss512 <wind855211@hotmail.com>
2023-03-06 21:55:32 +0800

    update cordova

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,7 @@
 </template>
 
 <script>
-import myToolbar from "@/components/MyToolbar"
 export default {
   name: 'app',
-  components: {myToolbar}
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 import Highschool_ke_leak from '@/views/Highschool_ke_leak'
-import Flash from '@/views/Flash'
 import Report from '@/views/Report'
-import Nfc from '@/views/Nfc'
-import Localisation from '@/views/Localisation'
 
 Vue.use(Router);
 
@@ -15,26 +12,14 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },{
+    }, {
       path: '/Highschool_ke_leak',
       name: 'Highschool_ke_leak',
       component: Highschool_ke_leak
-    },{
-      path: '/flash',
-      name: 'Flash',
-      component: Flash
-    },{
+    }, {
       path: '/Report',
       name: 'Report',
       component: Report
-    },{
-      path: '/nfc',
-      name: 'Nfc',
-      component: Nfc
-    },{
-      path: '/localisation',
-      name: 'Localisation',
-      component: Localisation
     }
   ]
 })
